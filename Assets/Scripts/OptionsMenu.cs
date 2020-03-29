40827167df96e60dfe784b845a5bdaaa3b29eeed
mccaua79 <drewmccauley90@gmail.com>
2020-03-29 15:57:36 -0500

    Adding options menu functionality, changing screen sizes, and making the volume slider report data

---
+++ Assets/Scripts/OptionsMenu.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class OptionsMenu : MonoBehaviour
+{
+    public void ChangeVolume(float value)
+    {
+        Debug.Log($"Volume changed: {value}");
+
+    }
+
+    public void SetScreenSize(int screenSize)
+    {
+        Debug.Log("Set Screen Size");
+        switch (screenSize)
+        {
+            case 1:
+                Screen.SetResolution(1024, 768, FullScreenMode.Windowed);
+                break;
+            case 2:
+                Screen.SetResolution(1200, 800, FullScreenMode.Windowed);
+                break;
+            case 3:
+                Screen.SetResolution(1600, 900, FullScreenMode.Windowed);
+                break;
+        }
+    }
+}
