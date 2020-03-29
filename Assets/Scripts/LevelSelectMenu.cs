1dcc67eba87d5b133be1380649d9cc1d64cb1634
mccaua79 <drewmccauley90@gmail.com>
2020-03-29 16:10:36 -0500

    Making a scroll view for the level select levels

---
+++ Assets/Scripts/LevelSelectMenu.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelSelectMenu : MonoBehaviour
+{
+    public int NumberOfLevels = 20;
+
+    public void PlayGame()
+    {
+        Debug.Log("Play Game");
+        SceneManager.LoadScene("GameScene");
+    }
+
+    public void LoadLevels()
+    {
+        // TODO: Dynamically add buttons to screen based on NumberOfLevels
+    }
+}
--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,14 +1,7 @@
 ﻿using UnityEngine;
-using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    public void PlayGame()
-    {
-        Debug.Log("Play Game");
-        SceneManager.LoadScene("GameScene");
-    }
-
     public void QuitGame()
     {
         Debug.Log("Quit");
