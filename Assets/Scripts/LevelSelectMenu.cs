bce2ded3e07e429e6b6313290c2c3719ffca8fef
Andrew McCauley <>
2020-03-31 21:43:41 -0500

    Level Button prefab to dynamically add to content

--- Assets/Scripts/LevelSelectMenu.cs
+++ Assets/Scripts/LevelSelectMenu.cs
@@ -1,9 +1,12 @@
-﻿using UnityEngine;
+﻿using TMPro;
+using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class LevelSelectMenu : MonoBehaviour
 {
     public int NumberOfLevels = 20;
+    public GameObject ViewPortContent;
+    public GameObject LevelButton;
 
     public void PlayGame()
     {
@@ -11,8 +14,29 @@ public class LevelSelectMenu : MonoBehaviour
         SceneManager.LoadScene("GameScene");
     }
 
+    public void Start()
+    {
+        LoadLevels();
+    }
+
     public void LoadLevels()
     {
-        // TODO: Dynamically add buttons to screen based on NumberOfLevels
+        // BUG: This is not deleting the correct items
+        var children = ViewPortContent.GetComponentsInChildren<Transform>();
+
+        // Clear out children in content
+        //foreach (var childObject in children)
+        //{
+        //    Destroy(childObject.gameObject);
+        //}
+
+        for (var i = 0; i < NumberOfLevels; i++)
+        {
+            var button = Instantiate(LevelButton, ViewPortContent.transform);
+            var text = button.GetComponentInChildren<TMP_Text>();
+            text.text = $"Level {i + 1}";
+        }
+
+        // TODO: Expand the content width based on # of levels
     }
 }
