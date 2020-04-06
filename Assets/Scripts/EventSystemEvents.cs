4d1c941957b8c42382249d61e3858a89fd755be4
mccaua79 <drewmccauley90@gmail.com>
2020-04-05 19:47:10 -0500

    Working on the menu selection with keyboard and gamepad (works on the first menu, just need to reset the selected based on which menu is open)

---
+++ Assets/Scripts/EventSystemEvents.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+[RequireComponent(typeof(EventSystem))]
+public class EventSystemEvents : MonoBehaviour
+{
+    public static event Action<GameObject, GameObject> OnSelectionUpdated;
+
+    [SerializeField]
+    private EventSystem _eventSystem;
+    private GameObject _lastSelectedGameObject;
+    
+    private static void DispatchSelectionUpdated(GameObject newSelectedGameObject, GameObject previousSelectedGameObject)
+    {
+        Debug.Log("OnSelectionUpdated");
+        OnSelectionUpdated?.Invoke(newSelectedGameObject, previousSelectedGameObject);
+    }
+
+
+    private void Update()
+    {
+        var currentSelectedGameObject = _eventSystem.currentSelectedGameObject;
+        if (currentSelectedGameObject != _lastSelectedGameObject)
+        {
+            DispatchSelectionUpdated(currentSelectedGameObject, _lastSelectedGameObject);
+            _lastSelectedGameObject = currentSelectedGameObject;
+        }
+    }
+}
